5b51927ff122aa0ec258f542a9b047046cb3c8a3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-24 16:18:05 +0530

    Done with the logic

--- 04_Age_Calculator/Solution.js
+++ 04_Age_Calculator/Solution.js
@@ -1,3 +1,33 @@
-const date = document.querySelector('#date')
-const result = document.querySelector('#result')
+
+const button = document.querySelector('#calculate')
+
+
+
+button.addEventListener('click', (e) => {
+    const date = document.querySelector('#date').value
+    const todayDate = new Date()
+    const result = document.querySelector('#result')
+
+    console.log(date);
+
+    if (date == "") {
+        console.log("inside");
+        result.innerHTML = `Please enter your age`
+    } else {
+        const dateValue = new Date(date)
+
+        let age = todayDate.getFullYear() - dateValue.getFullYear()
+        const monthDiff = todayDate.getMonth() - dateValue.getMonth()
+        const dateDiff = todayDate.getDate() - dateValue.getDate()
+
+        if (monthDiff < 0 || (monthDiff == 0 && dateDiff < 0)) {
+            age--;
+        }
+
+        result.innerHTML = `Your age is ${age}`
+    }
+    
+})
+
+
 
