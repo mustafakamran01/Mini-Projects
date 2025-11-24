82d93edb94d3c78c11c2dd5086fd5461317be9f3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-24 10:30:37 +0530

    Adding logics

--- 03_Testimonial_Slider/Solution.js
+++ 03_Testimonial_Slider/Solution.js
@@ -1,34 +1,56 @@
-const content = [
-    `A rose by any other name would smell as sweet.
-    All the world's a stage, and all the men and women merely players.
+// const content = [
+//     `A rose by any other name would smell as sweet.
+//     All the world's a stage, and all the men and women merely players.
 
-    Kamran`,
+//     Kamran`,
 
-    `A rose by any other name would smell as sweet.
-    All the world's a stage, and all the men and women merely players.
+//     `A rose by any other name would smell as sweet.
+//     All the world's a stage, and all the men and women merely players.
 
-    Mustafa`,
+//     Mustafa`,
 
-    `A rose by any other name would smell as sweet.
-    All the world's a stage, and all the men and women merely players.
+//     `A rose by any other name would smell as sweet.
+//     All the world's a stage, and all the men and women merely players.
 
-    Hello`,
+//     Hello`,
 
-    `A rose by any other name would smell as sweet.
-    All the world's a stage, and all the men and women merely players.
+//     `A rose by any other name would smell as sweet.
+//     All the world's a stage, and all the men and women merely players.
 
-    World`
-]
+//     World`
+// ]
 
-const mainDiv = document.querySelector('#main')
+// const mainDiv = document.querySelector('#main')
+
+// let i = 0;
+
+// const ref = setInterval( () => {
+//     mainDiv.innerHTML = content[i];
+//     i++;
+
+//     if (i >= content.length) {
+//     clearInterval(ref)
+//     }
+// }, 2000)
+
+const innerDiv = document.querySelector('#inner')
+
+const para1 = document.querySelector('#one').innerHTML
+const para2 = document.querySelector('#two').innerHTML
+const para3 = document.querySelector('#three').innerHTML
+const para4 = document.querySelector('#four').innerHTML
+
+const arr = Array.of(para1, para2, para3, para4)
 
 let i = 0;
 
 const ref = setInterval( () => {
-    mainDiv.innerHTML = content[i];
+    innerDiv.innerHTML = arr[i];
+    console.log(arr[i]);
     i++;
 
-    if (i >= content.length) {
-    clearInterval(ref)
+    if (i >= arr.length) {
+    // clearInterval(ref)
+    i = 0
     }
 }, 2000)
