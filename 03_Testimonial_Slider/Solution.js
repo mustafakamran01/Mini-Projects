0da6a579e3e010ec87994a65230c0e015e5665d4
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-24 11:01:36 +0530

    Done with the code

--- 03_Testimonial_Slider/Solution.js
+++ 03_Testimonial_Slider/Solution.js
@@ -1,60 +1,3 @@
-// const content = [
-//     `A rose by any other name would smell as sweet.
-//     All the world's a stage, and all the men and women merely players.
-
-//     Kamran`,
-
-//     `A rose by any other name would smell as sweet.
-//     All the world's a stage, and all the men and women merely players.
-
-//     Mustafa`,
-
-//     `A rose by any other name would smell as sweet.
-//     All the world's a stage, and all the men and women merely players.
-
-//     Hello`,
-
-//     `A rose by any other name would smell as sweet.
-//     All the world's a stage, and all the men and women merely players.
-
-//     World`
-// ]
-
-// const mainDiv = document.querySelector('#main')
-
-// let i = 0;
-
-// const ref = setInterval( () => {
-//     mainDiv.innerHTML = content[i];
-//     i++;
-
-//     if (i >= content.length) {
-//     clearInterval(ref)
-//     }
-// }, 2000)
-
-// const innerDiv = document.querySelector('#inner')
-
-// const para1 = document.querySelector('#one').innerHTML
-// const para2 = document.querySelector('#two').innerHTML
-// const para3 = document.querySelector('#three').innerHTML
-// const para4 = document.querySelector('#four').innerHTML
-
-// const arr = Array.of(para1, para2, para3, para4)
-
-// let i = 0;
-
-// const ref = setInterval( () => {
-//     innerDiv.innerHTML = arr[i];
-//     console.log(arr[i]);
-//     i++;
-
-//     if (i >= arr.length) {
-//     // clearInterval(ref)
-//     i = 0
-//     }
-// }, 2000)
-
 const list_of_content = [
     {
         imgUrl: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=880&q=80',
@@ -76,7 +19,6 @@ const list_of_content = [
 ]
 
 
-
 const img = document.querySelector('#img')
 const content = document.querySelector('#content')
 const name = document.querySelector('#name')
@@ -93,9 +35,9 @@ function testimonial() {
 
     i++
 
-    // if (i == list_of_content.length) {
-    //     i = 0
-    // }
+    if (i == list_of_content.length) {
+        i = 0
+    }
 }
 
 setInterval( () => {
