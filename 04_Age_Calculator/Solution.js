bbb97dc475a78f6a1c40aaea3571fca6b0d0f8db
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-25 17:33:18 +0530

    Done with the project 4

--- 04_Age_Calculator/Solution.js
+++ 04_Age_Calculator/Solution.js
@@ -1,4 +1,27 @@
+document.querySelector('#calculate').addEventListener( 'click', (e) => {
 
+    const DOB = document.querySelector('#date').value
+    const result = document.querySelector('#result')
+
+    const todayDate = new Date();
+    const DOBvalue = new Date(DOB)
+
+    if (DOB == "") {
+        result.innerHTML = "Please enter your age"
+    } else {
+        
+        let age = todayDate.getFullYear() - DOBvalue.getFullYear()
+        const monthDiff = todayDate.getMonth() - DOBvalue.getMonth()
+        const dateDiff = todayDate.getDate() - DOBvalue.getDate()
+
+        if (monthDiff < 0 || (monthDiff == 0 && dateDiff < 0)) {
+            age--;
+        }
+
+        result.innerHTML = `Your age is ${age}`
+    }
+
+})
 
 
 
