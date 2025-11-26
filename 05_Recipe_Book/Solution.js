911e7f9b9460f12ea24adf930e61bc9a654b14bb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-26 12:43:03 +0530

    Done with the logics

--- 05_Recipe_Book/Solution.js
+++ 05_Recipe_Book/Solution.js
@@ -48,6 +48,46 @@
 // }
 
 
+const recipeList = document.querySelector('#recipeList')
+
+fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=275d58779ccf4e22af03e792e8819fff')
+.then( (response) => response.json())
+.then( (response) => recipe(response.recipes))
+
+function recipe(retrivedRecipe) {
+
+    retrivedRecipe.map( (element) => {
+
+        const myLI = document.createElement('li')
+        myLI.className = "recipe-item"
+
+        const myImg = document.createElement('img')
+        myImg.src = element.image
+        myImg.alt = "View Image"
+
+        const myH2 = document.createElement('h2')
+        myH2.innerHTML = element.title
+
+        const myPara = document.createElement('p')
+        myPara.innerHTML = `<strong>Ingredients: </strong> 
+        ${element.extendedIngredients.map( (ingredient) => ingredient.original).join(", ")}`
+
+        const link = document.createElement('a')
+        link.href = element.sourceURL
+        link.innerHTML = "View Recipe"
+
+        myLI.appendChild(myImg)
+        myLI.appendChild(myH2)
+        myLI.appendChild(myPara)
+        myLI.appendChild(link)
+
+        recipeList.appendChild(myLI)
+    })
+}
+
+
+
+
 
 
 
