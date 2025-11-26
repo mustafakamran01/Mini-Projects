568c18040eb09be391322fc250fad581cbfd94f0
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-26 18:32:16 +0530

    Done with the logic

--- 07_Pomodoro_Timer/Solution.js
+++ 07_Pomodoro_Timer/Solution.js
@@ -1,12 +1,49 @@
-const timer = document.querySelector('#timer').textContent
-console.log(timer);
+const timer = document.querySelector('#timer')
 
 const start = document.querySelector('#start')
 const stop = document.querySelector('#stop')
 const reset = document.querySelector('#reset')
 
-const [minutes, seconds] = timer.split(':').map(Number)
+let [minutes, seconds] = timer.textContent.split(':').map(Number)
+let interval = null;
 
 start.addEventListener('click', (e) => {
+    if (interval == null) {
+
+        interval = setInterval( () => {
+        if (seconds == 0) {
+            if (minutes == 0) {
+                timer.innerHTML = "00:00"
+                clearInterval(interval)
+            }
+            minutes--;
+            seconds = 59;
+        } else{
+            seconds--;
+        }
+        
+
+        let mm = String(minutes).padStart(2, 0)
+        let ss = String(seconds).padStart(2, 0)
+
+        timer.innerHTML = `${mm}:${ss}`
+        }, 1000)
+    }
+})
+
+console.log("Kamran");
+
+
+stop.addEventListener('click', (e) => {
+    clearInterval(interval)
+    interval = null
+})
+
+reset.addEventListener('click', (e) => {
+    clearInterval(interval)
+    interval = null;
+    timer.innerHTML = "25:00"
+    minutes = 25
+    seconds = 0
 
 })
