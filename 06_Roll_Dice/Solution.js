d64e8366eb3a137a7836f47f71dbd442767920d3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-26 10:40:39 +0530

    Understanding dice concept

--- 06_Roll_Dice/Solution.js
+++ 06_Roll_Dice/Solution.js
@@ -7,10 +7,6 @@ button.addEventListener('click', (e) => {
 
     const randomNumber = Math.floor(Math.random() * 6 + 1)
 
-    // console.log(randomNumber);
-
-    
-
     const myDiv = document.createElement('li')
     myDiv.innerHTML = `Roll ${i} : ${randomNumber}`
     i++;
