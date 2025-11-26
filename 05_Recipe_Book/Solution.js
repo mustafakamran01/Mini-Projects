d79ddf3f95e62e80857689113b9766de58a91297
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-26 12:45:29 +0530

    Labelling and arranging codes

--- 05_Recipe_Book/Solution.js
+++ 05_Recipe_Book/Solution.js
@@ -1,61 +1,15 @@
-// const recipeList = document.querySelector('#recipeList')
-
-// async function retriveRecipe() {
-//     const response = await fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=275d58779ccf4e22af03e792e8819fff');
-//     const data = await response.json();
-
-//     // return data.recipes;
-//     recipe(data.recipes)
-// }
-
-// // async function starter() {
-// //     const retrivedRecipe = await retriveRecipe()
-// //     recipe(retrivedRecipe)
-// // }
-
-// // starter();
-
-// retriveRecipe();
-
-// function recipe(retrivedRecipe) {
-//     retrivedRecipe.map( (element) => {
-//         const myLI = document.createElement('li')
-//         myLI.className = "recipe-item"
-
-//         const myImg = document.createElement('img')
-//         myImg.src = element.image
-//         myImg.alt = "View Image"
-
-//         const myH2 = document.createElement('h2')
-//         myH2.innerHTML = element.title
-
-//         const myPara = document.createElement('p')
-//         myPara.innerHTML = `<strong>Ingredients: </strong> ${element.extendedIngredients.map( (ingredients) => 
-//             ingredients.original).join(",")}`
-
-//         const link = document.createElement('a')
-//         link.href= element.sourceUrl
-//         link.innerText = "View Recipe"
-
-//         myLI.appendChild(myImg)
-//         myLI.appendChild(myH2)
-//         myLI.appendChild(myPara)
-//         myLI.appendChild(link)
-
-//         recipeList.appendChild(myLI)
-
-//     })
-// }
-
-
+// Accessing the un-ordered list
 const recipeList = document.querySelector('#recipeList')
 
+// Extracting the data from the given api
 fetch('https://api.spoonacular.com/recipes/random?number=10&apiKey=275d58779ccf4e22af03e792e8819fff')
 .then( (response) => response.json())
 .then( (response) => recipe(response.recipes))
 
+// function to display the recipes
 function recipe(retrivedRecipe) {
 
+    // Iterate over the each food item 
     retrivedRecipe.map( (element) => {
 
         const myLI = document.createElement('li')
