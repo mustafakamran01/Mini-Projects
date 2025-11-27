2b4e1b338a309a74fb66857470181afee823aabb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-27 12:18:50 +0530

    Arranging and labelling codes of project 4

--- 04_Age_Calculator/Solution.js
+++ 04_Age_Calculator/Solution.js
@@ -1,27 +1,26 @@
-document.querySelector('#calculate').addEventListener( 'click', (e) => {
+// Accessing button and result paragraph 
+const button = document.querySelector('#calculate')
+const result = document.querySelector('#result')
 
-    const DOB = document.querySelector('#date').value
-    const result = document.querySelector('#result')
+// Adding eventListener to button to calculate the age
+button.addEventListener('click', (e) => {
 
-    const todayDate = new Date();
-    const DOBvalue = new Date(DOB)
+    // Accessing input value(DOB) and converting it into new Date()
+    const DOB = new Date(document.querySelector('#date').value)
+    const todayDate = new Date()
 
+    // If DOB is empty, then we will show an error message otherwise calculating age and displaying it into result paragraph
     if (DOB == "") {
         result.innerHTML = "Please enter your age"
     } else {
-        
-        let age = todayDate.getFullYear() - DOBvalue.getFullYear()
-        const monthDiff = todayDate.getMonth() - DOBvalue.getMonth()
-        const dateDiff = todayDate.getDate() - DOBvalue.getDate()
+        let age = todayDate.getFullYear() - DOB.getFullYear()
+        const monthDiff = todayDate.getMonth() - DOB.getMonth()
+        const dateDiff = todayDate.getDate() - DOB.getDate()
 
         if (monthDiff < 0 || (monthDiff == 0 && dateDiff < 0)) {
             age--;
         }
 
         result.innerHTML = `Your age is ${age}`
-    }
-
+        }
 })
-
-
-
