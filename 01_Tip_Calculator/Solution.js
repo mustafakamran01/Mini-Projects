6cfd2629d50b9701e4bb8a2fd23c0dbb4799cab2
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-27 10:37:51 +0530

    Arranging and labelling codes of project 1

--- 01_Tip_Calculator/Solution.js
+++ 01_Tip_Calculator/Solution.js
@@ -1,14 +1,17 @@
-const mainDiv = document.querySelector('#main')
-
+// Adding eventListener to the button to calculate the total amount
 document.querySelector('#calculate').addEventListener('click', (e) => {
 
+    // Taking values from bill amount and tip percentage
     const billAmount = Number(document.querySelector('#amount').value)
     const tipPercent = Number(document.querySelector('#tip').value)
+
+    // Accessing the result paragraph to display the output
     const result = document.querySelector('#result')
-        
+    
+    // Calculating tip amount and total amount
     const tipAmount = (billAmount * tipPercent)/100
     const totalAmount = billAmount + tipAmount
 
+    // Displaying the total amount in the result paragraph
     result.innerHTML = `Total Amount: $${totalAmount}`
-
 })
