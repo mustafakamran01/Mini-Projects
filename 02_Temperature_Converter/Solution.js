d6a0a02b2424ea88d015538ca6ac9171ba2d35de
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-27 10:32:33 +0530

    Labelling and arranging codes of project 2

--- 02_Temperature_Converter/Solution.js
+++ 02_Temperature_Converter/Solution.js
@@ -1,18 +1,21 @@
 function temperatureConversion() {
+    // Accessing input and output fields
     const inputField = document.querySelector('#input')
     const outputField = document.querySelector('#result')
 
+    // Accessing input and output options
     const inputOption = document.querySelector('#inputOption')
     const outputOption = document.querySelector('#outputOption')
 
-
+    // Taking value from the input field and input option
     let input = Number(inputField.value)
     let inputSelect = inputOption.value
     
-
-    let outputSelect = outputOption.value
+    // Taking value from output options
     let output = 0;
+    let outputSelect = outputOption.value
 
+    // Depending upon the input option and output option we will further proceed and calculate the temperature
     if (inputSelect == "Celsius") {
         if (outputSelect == "Fahrenheit") {
             output = (input * (9/5)) + 32
@@ -39,9 +42,11 @@ function temperatureConversion() {
         }
     }
 
+    // Inserting the calculated value into the output field
     outputField.value = output
 }
 
+// Adding eventListener to input field, input option and output option
 document.querySelector('#input').addEventListener('input', temperatureConversion)
 document.querySelector('#inputOption').addEventListener('change', temperatureConversion)
 document.querySelector('#outputOption').addEventListener('change', temperatureConversion)
