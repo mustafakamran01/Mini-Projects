ab83ca966867ad13457a1c44cfdbc20d96890a86
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-27 16:02:18 +0530

    Arranging and labelling codes of project 6

--- 06_Roll_Dice/Solution.js
+++ 06_Roll_Dice/Solution.js
@@ -1,96 +1,91 @@
+// Accessing un-ordered list, button and dice logo
+const rollHistory = document.querySelector('#roll-history')
 const button = document.querySelector('#roll-button')
-const history = document.querySelector('#roll-history')
 const dice = document.querySelector('#dice')
 
-let i = 1
+let i = 1;
 
+// Adding eventListener on button to generate random dice face
 button.addEventListener('click', (e) => {
     dice.classList.add("roll-animation");
 
      setTimeout(() => {
         dice.classList.remove("roll-animation");
-        // displayDice();
     }, 1000);
 
-    const randomNumber = Math.floor(Math.random() * 6 + 1)
-    console.log(randomNumber);
+    const randomNumber =  Math.floor((Math.random() * 6) + 1)
 
     displayDice(randomNumber)
-    
 })
 
+// Function to display random dice face
 function displayDice(randomNumber) {
     const userChoice = randomNumber
 
     switch (userChoice) {
         case 1:
-            dice.innerHTML = `&#9856`
-            const myLi1 = document.createElement('li')            
+            const myLi1 = document.createElement('li')
+            myLi1.innerHTML = `Roll ${i}: `
             const mySpan1 = document.createElement('span')
-            myLi1.appendChild(document.createTextNode(`Roll ${i}`))
-            mySpan1.innerHTML = `&#9856`
+            mySpan1.innerHTML = `&#9856;`
             myLi1.appendChild(mySpan1)
-            history.appendChild(myLi1)
+            rollHistory.appendChild(myLi1)
+            dice.innerHTML = `&#9856;`
             i++
             break;
-
+        
         case 2:
-            dice.innerHTML = `&#9857`
-            const myLi2 = document.createElement('li')            
+            const myLi2 = document.createElement('li')
+            myLi2.innerHTML = `Roll ${i}: `
             const mySpan2 = document.createElement('span')
-            myLi2.appendChild(document.createTextNode(`Roll ${i}`))
-            mySpan2.innerHTML = `&#9857`
+            mySpan2.innerHTML = `&#9857;`
             myLi2.appendChild(mySpan2)
-            history.appendChild(myLi2)
+            rollHistory.appendChild(myLi2)
+            dice.innerHTML = `&#9857;`
             i++
             break;
-
         case 3:
-            dice.innerHTML = `&#9858`
-            const myLi3 = document.createElement('li')            
+            const myLi3 = document.createElement('li')
+            myLi3.innerHTML = `Roll ${i}: `
             const mySpan3 = document.createElement('span')
-            myLi3.appendChild(document.createTextNode(`Roll ${i}`))
-            mySpan3.innerHTML = `&#9858`
+            mySpan3.innerHTML = `&#9858;`
             myLi3.appendChild(mySpan3)
-            history.appendChild(myLi3)
+            rollHistory.appendChild(myLi3)
+            dice.innerHTML = `&#9858;`
             i++
             break;
 
         case 4:
-            dice.innerHTML = `&#9859`
-            const myLi4 = document.createElement('li')            
+            const myLi4 = document.createElement('li')
+            myLi4.innerHTML = `Roll ${i}: `
             const mySpan4 = document.createElement('span')
-            myLi4.appendChild(document.createTextNode(`Roll ${i}`))
-            mySpan4.innerHTML = `&#9859`
+            mySpan4.innerHTML = `&#9859;`
             myLi4.appendChild(mySpan4)
-            history.appendChild(myLi4)
+            rollHistory.appendChild(myLi4)
+            dice.innerHTML = `&#9859;`
             i++
             break;
 
         case 5:
-            dice.innerHTML = `&#9860`
-            const myLi5 = document.createElement('li')            
+            const myLi5 = document.createElement('li')
+            myLi5.innerHTML = `Roll ${i}: `
             const mySpan5 = document.createElement('span')
-            myLi5.appendChild(document.createTextNode(`Roll ${i}`))
-            mySpan5.innerHTML = `&#9860`
+            mySpan5.innerHTML = `&#9860;`
             myLi5.appendChild(mySpan5)
-            history.appendChild(myLi5)
+            rollHistory.appendChild(myLi5)
+            dice.innerHTML = `&#9860;`
             i++
             break;
 
         case 6:
-            dice.innerHTML = `&#9861`
-            const myLi6 = document.createElement('li')            
+            const myLi6 = document.createElement('li')
+            myLi6.innerHTML = `Roll ${i}: `
             const mySpan6 = document.createElement('span')
-            myLi6.appendChild(document.createTextNode(`Roll ${i}`))
-            mySpan6.innerHTML = `&#9861`
+            mySpan6.innerHTML = `&#9861;`
             myLi6.appendChild(mySpan6)
-            history.appendChild(myLi6)
+            rollHistory.appendChild(myLi6)
+            dice.innerHTML = `&#9861;`
             i++
-            break;
-    
-        default:
-
             break;
     }
 }
