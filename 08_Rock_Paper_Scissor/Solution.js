aebab16acd7f42a8e756a3d024bc9523445fbcb3
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-28 22:52:58 +0530

    Done with the logic

---
+++ 08_Rock_Paper_Scissor/Solution.js
@@ -0,0 +1,71 @@
+const buttons = document.querySelector('.buttons')
+
+const result = document.querySelector('#result')
+
+const userScore = document.querySelector('#user-score')
+const computerScore = document.querySelector('#computer-score')
+
+let userScoreNumber = 0;
+let computerScoreNumber = 0;
+
+
+buttons.addEventListener( 'click', (e) => {
+
+    let computerChoice = Math.floor((Math.random() * 3) + 1)
+    let id = e.target.id
+
+    switch (computerChoice) {
+        
+        // 1 -> rock
+        case 1:
+
+            if (id == "rock" && computerChoice == 1) {
+                result.innerHTML = "It's a tie!"
+            } else if (id == "paper" && computerChoice == 1) {
+                result.innerHTML = "You win! paper beats rock"
+                userScoreNumber++
+            } else if (id == "scissors" && computerChoice == 1) {
+                result.innerHTML = "You lose! rock beats scissor"
+                computerScoreNumber++
+            }
+
+            break;
+
+        // 2 -> paper
+        case 2:
+
+            if (id == "rock" && computerChoice == 2) {
+                result.innerHTML = "You lose! paper beats rock"
+                computerScoreNumber++
+            } else if (id == "paper" && computerChoice == 2) {
+                result.innerHTML = "It's a tie!"
+            } else if (id == "scissors" && computerChoice == 2) {
+                result.innerHTML = "You win! scissor beats paper"
+                userScoreNumber++
+            }
+
+            break;
+
+        // 3 -> scissor
+        case 3:
+
+            if (id == "rock" && computerChoice == 3) {
+                result.innerHTML = "You win! rock beats scissor"
+                userScoreNumber++
+            } else if (id == "paper" && computerChoice == 3) {
+                result.innerHTML = "You lose! scissor beats paper"
+                computerScoreNumber++
+            } else if (id == "scissors" && computerChoice == 3) {
+                result.innerHTML = "It's a tie!"
+            }
+
+            break;
+
+        default:
+            break;
+    }
+    
+    userScore.innerHTML = userScoreNumber
+    computerScore.innerHTML = computerScoreNumber
+
+})
