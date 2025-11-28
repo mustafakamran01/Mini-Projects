e297cad4cbbde3cfffcfefe5569170290751bdf2
mustafakamran01 <kamran000mustafa@gmail.com>
2025-11-28 19:25:25 +0530

    Applying changes on project 7

--- 07_Pomodoro_Timer/Solution.js
+++ 07_Pomodoro_Timer/Solution.js
@@ -1,4 +1,4 @@
-// Accessing paragraph to show the timer
+// Accessing paragraph to display the timer
 const timer = document.querySelector('#timer')
 
 // Accessing all the buttons
@@ -6,45 +6,50 @@ const start = document.querySelector('#start')
 const stop = document.querySelector('#stop')
 const reset = document.querySelector('#reset')
 
-// Converting timer string to number and spliting on the basis of ':'
+// Spliting the time into minutes ans seconds
 let [minutes, seconds] = timer.textContent.split(':').map(Number)
-let interval = null;
+let interval = null
 
-// Adding eventListener to the start button to start the timer
+// Adding eventListener to start button to start the timer
 start.addEventListener('click', (e) => {
+
     if (interval == null) {
 
         interval = setInterval( () => {
         if (seconds == 0) {
             if (minutes == 0) {
-                timer.innerHTML = "00:00"
+                timer.innerHTML = `00:00`
                 clearInterval(interval)
             }
             minutes--;
-            seconds = 59;
-        } else{
+            seconds = 59
+        } else {
             seconds--;
         }
-        
+
         let mm = String(minutes).padStart(2, 0)
         let ss = String(seconds).padStart(2, 0)
 
-        timer.innerHTML = `${mm}:${ss}`
+        console.log(typeof(mm));
+        console.log(ss)
+
+        timer.textContent = `${mm} : ${ss}`
+
         }, 1000)
     }
 })
 
-// Adding eventListener to the stop button to stop the timer
+// Adding eventListener to stop button to stop the timer
 stop.addEventListener('click', (e) => {
     clearInterval(interval)
     interval = null
 })
 
-// Adding eventListener to reset button to reset all the times and paragraph 
+// Adding eventListener to reset button to reset the timer
 reset.addEventListener('click', (e) => {
     clearInterval(interval)
-    interval = null;
-    timer.innerHTML = "25:00"
     minutes = 25
     seconds = 0
+    timer.innerHTML = '25 : 00'
+    interval = null
 })
