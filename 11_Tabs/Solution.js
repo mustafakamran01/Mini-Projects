c362ee018e54bf9fb1ff45ebf85060111c1ad954
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-01 10:54:07 +0530

    Applying different approach for project 11

--- 11_Tabs/Solution.js
+++ 11_Tabs/Solution.js
@@ -1,62 +1,64 @@
-const buttons = document.querySelector('.btn-container')
+// const buttons = document.querySelector('.btn-container')
 
-const tab1 = document.querySelector('#step1')
-const tab2 = document.querySelector('#step2')
-const tab3 = document.querySelector('#step3')
+// const tab1 = document.querySelector('#step1')
+// const tab2 = document.querySelector('#step2')
+// const tab3 = document.querySelector('#step3')
 
-buttons.addEventListener('click', (e) => {
-    let id = e.target.id
+// buttons.addEventListener('click', (e) => {
+//     let id = e.target.id
 
-    console.log(id);
+//     console.log(id);
 
-    switch (id) {
-        case 'button1':
-            let button = document.querySelector('#button1')
+//     switch (id) {
+//         case 'button1':
+//             let button = document.querySelector('#button1')
 
-            if (!tab1.classList.contains('live')) {
+//             if (!tab1.classList.contains('live')) {
 
-                if (tab2.classList.contains('live')) {
-                    tab2.classList.remove('live')
-                }
+//                 if (tab2.classList.contains('live')) {
+//                     tab2.classList.remove('live')
+//                 }
 
-                if (tab3.classList.contains('live')) {
-                    tab3.classList.remove('live')
-                }
+//                 if (tab3.classList.contains('live')) {
+//                     tab3.classList.remove('live')
+//                 }
 
-                tab1.classList.add('live')
-            }
-            break;
+//                 tab1.classList.add('live')
+//             }
+//             break;
 
-        case 'button2':
-            if (!tab2.classList.contains('live')) {
+//         case 'button2':
+//             if (!tab2.classList.contains('live')) {
 
-                if (tab1.classList.contains('live')) {
-                    tab1.classList.remove('live')
-                }
+//                 if (tab1.classList.contains('live')) {
+//                     tab1.classList.remove('live')
+//                 }
 
-                if (tab3.classList.contains('live')) {
-                    tab3.classList.remove('live')
-                }
+//                 if (tab3.classList.contains('live')) {
+//                     tab3.classList.remove('live')
+//                 }
                 
-                tab2.classList.add('live')
-            }
-            break;
+//                 tab2.classList.add('live')
+//             }
+//             break;
 
-        case 'button3':
-            if (!tab3.classList.contains('live')) {
+//         case 'button3':
+//             if (!tab3.classList.contains('live')) {
 
-                if (tab1.classList.contains('live')) {
-                    tab1.classList.remove('live')
-                }
+//                 if (tab1.classList.contains('live')) {
+//                     tab1.classList.remove('live')
+//                 }
 
-                if (tab2.classList.contains('live')) {
-                    tab2.classList.remove('live')
-                }
-                tab3.classList.add('live')
-            }
-            break;
+//                 if (tab2.classList.contains('live')) {
+//                     tab2.classList.remove('live')
+//                 }
+//                 tab3.classList.add('live')
+//             }
+//             break;
     
-        default:
-            break;
-    }
-})
+//         default:
+//             break;
+//     }
+// })
+
+
