e1592941049c1cd8c3cf778de8df85822057c58a
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-01 20:57:36 +0530

    Structuring project 12

---
+++ 12_Sticky_Navbar/Solution.js
@@ -0,0 +1,6 @@
+const navbar = document.querySelector('.navbar')
+const top_container = document.querySelector('.top-container')
+const bottom_container = document.querySelector('.bottom-container')
+
+
+
