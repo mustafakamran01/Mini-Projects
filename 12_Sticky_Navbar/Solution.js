f9ccb7b694bf1e739235dc9894be4237b1b076af
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-02 10:50:09 +0530

    Done with the logic

--- 12_Sticky_Navbar/Solution.js
+++ 12_Sticky_Navbar/Solution.js
@@ -4,3 +4,8 @@ const bottom_container = document.querySelector('.bottom-container')
 
 
 
+window.addEventListener('scroll', (e) => {
+    if (window.scrollY > (bottom_container.offsetTop - navbar.offsetHeight)) {
+        navbar.classList.add('active')
+    }
+})
