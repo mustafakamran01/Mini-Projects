cf2d7d4c3c87cf4270fc7a6f374b4d84fcaea8d4
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-02 11:13:33 +0530

    Arranging and labelling codes

--- 12_Sticky_Navbar/Solution.js
+++ 12_Sticky_Navbar/Solution.js
@@ -1,14 +1,16 @@
+// Accessing navbar and bottom-container
 const navbar = document.querySelector('.navbar')
-const top_container = document.querySelector('.top-container')
 const bottom_container = document.querySelector('.bottom-container')
 
-
-
+// Adding eventListener to the window
 window.addEventListener('scroll', (e) => {
+    
+    // If navbar reaches the bottom-container, then add 'active' to the classList
     if (window.scrollY > (bottom_container.offsetTop - navbar.offsetHeight) - 50) {
         navbar.classList.add('active')
     }
 
+    // If not, then remove the 'active' from the classList
     if (window.scrollY < (bottom_container.offsetTop - navbar.offsetHeight) - 50) {
         navbar.classList.remove('active')
     }
