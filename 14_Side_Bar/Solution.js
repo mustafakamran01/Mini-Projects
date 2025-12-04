2b97f892ba2bd3db27b7f9e97090ff08ddd521cb
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-04 12:19:58 +0530

    Arranging and labelling codes

--- 14_Side_Bar/Solution.js
+++ 14_Side_Bar/Solution.js
@@ -1,12 +1,14 @@
-//
+// Accessing bars, sidebar and closeIcon
 const bars = document.querySelector('.fa-bars')
 const sidebar = document.querySelector('.sidebar')
 const closeButton = document.querySelector('.fa-times')
 
+// Adding eventListener to bars to toggle sidebar
 bars.addEventListener('click', (e) => {
     sidebar.classList.toggle('show-sidebar')
 })
 
+// Adding eventListener to closeIcon to toggle sidebar
 closeButton.addEventListener('click', (e) => {
     sidebar.classList.toggle('show-sidebar')
 })
