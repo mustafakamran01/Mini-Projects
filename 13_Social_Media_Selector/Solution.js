2314950d6e3e65d1c05a4f7a1c7c997c1b52b98e
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-04 11:31:55 +0530

    Arranging and labelling codes

--- 13_Social_Media_Selector/Solution.js
+++ 13_Social_Media_Selector/Solution.js
@@ -1,15 +1,17 @@
-
+// Accessing menuDiv, menuParagraph, socialListDiv and all list-items
 const menu = document.querySelector('.menu')
 const menuPara = document.querySelector('.menu p')
 const social_list = document.querySelector('.social-lists')
 const liEls = document.querySelectorAll('.social-lists li')
 
+// Adding eventListener to menu to toggle the list-item
 menu.addEventListener('click', (e) => {
 
     social_list.classList.toggle('hide')
     menu.classList.toggle('rotate')
 })
 
+// Iterating over each list-item and adding eventListener to them
 liEls.forEach( (li) => {
     li.addEventListener('click', (e) => {
         menuPara.innerHTML = li.innerHTML
