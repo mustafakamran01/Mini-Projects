a54db65d09e35740d05bd6475ce98379feee631d
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-04 12:08:04 +0530

    Applying logic

--- 14_Side_Bar/Solution.js
+++ 14_Side_Bar/Solution.js
@@ -1,1 +1,6 @@
 const icon = document.querySelector('.fa-bars')
+const sidebar = document.querySelector('.sidebar')
+
+icon.addEventListener('click', (e) => {
+    sidebar.classList.toggle('show-sidebar')
+})
