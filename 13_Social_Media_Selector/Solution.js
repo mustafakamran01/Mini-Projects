214b4200ff5a26452c6a0769163601f2852b26f2
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-04 11:07:44 +0530

    Re-trying project 13

--- 13_Social_Media_Selector/Solution.js
+++ 13_Social_Media_Selector/Solution.js
@@ -1,20 +1,21 @@
-const menu = document.querySelector('.menu')
-const menuPara = document.querySelector('.menu p')
-const social_list = document.querySelector('.social-lists')
-const liEls = document.querySelectorAll('.social-lists li')
+// const menu = document.querySelector('.menu')
+// const menuPara = document.querySelector('.menu p')
+// const social_list = document.querySelector('.social-lists')
+// const liEls = document.querySelectorAll('.social-lists li')
 
-menu.addEventListener('click', (e) => {
+// menu.addEventListener('click', (e) => {
 
-    social_list.classList.toggle('hide')
-    menu.classList.toggle('rotate')
-})
+//     social_list.classList.toggle('hide')
+//     menu.classList.toggle('rotate')
+// })
 
-liEls.forEach( (li) => {
-    console.log("List item");
-    li.addEventListener('click', (e) => {
-        console.log("Inner item");
-        menuPara.innerHTML = li.innerHTML
-        menu.classList.toggle('rotate')
-        social_list.classList.add('hide')
-    })
-})
+// liEls.forEach( (li) => {
+//     console.log("List item");
+//     li.addEventListener('click', (e) => {
+//         console.log("Inner item");
+//         menuPara.innerHTML = li.innerHTML
+//         menu.classList.toggle('rotate')
+//         social_list.classList.add('hide')
+//     })
+// })
+
