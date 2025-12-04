836f5eb1314aa5a7adb1952e3c5d25e4388a83c1
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-04 12:09:42 +0530

    Adding CSS file

--- 14_Side_Bar/Solution.js
+++ 14_Side_Bar/Solution.js
@@ -1,6 +1,11 @@
 const icon = document.querySelector('.fa-bars')
 const sidebar = document.querySelector('.sidebar')
+const cross = document.querySelector('.fa-times')
 
 icon.addEventListener('click', (e) => {
     sidebar.classList.toggle('show-sidebar')
+})
+
+cross.addEventListener('click', (e) => {
+    
 })
