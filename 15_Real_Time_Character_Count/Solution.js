a36729962cc7136fecab7aee59f20bc37f9b033f
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-08 17:13:42 +0530

    Applying logic

--- 15_Real_Time_Character_Count/Solution.js
+++ 15_Real_Time_Character_Count/Solution.js
@@ -2,3 +2,27 @@ const textArea = document.querySelector('#textarea')
 const totalCounter = document.querySelector('#total-counter')
 const remainingCounter = document.querySelector('#remaining-counter')
 
+let totalCounterValue = 0
+let remainingCounterValue = 50
+
+totalCounter.innerHTML = totalCounterValue
+remainingCounter.innerHTML = remainingCounterValue
+
+textArea.addEventListener('keydown', (e) => {
+
+    if (textArea.value == "") {
+
+    } else if (e.key === 'Backspace') {
+        totalCounterValue--;
+        remainingCounterValue++;
+        totalCounter.innerHTML = totalCounterValue
+        remainingCounter.innerHTML = remainingCounterValue
+
+    } else {
+        totalCounterValue++
+        remainingCounterValue--;
+        totalCounter.innerHTML = totalCounterValue
+        remainingCounter.innerHTML = remainingCounterValue
+    }
+    
+})
