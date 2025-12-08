4e4e7f3d3e3e80fab076f1c2ac2c9c529bb063e9
mustafakamran01 <kamran000mustafa@gmail.com>
2025-12-08 16:49:35 +0530

    Structuring HTML and CSS for project 15

---
+++ 15_Real_Time_Character_Count/Solution.js
@@ -0,0 +1,4 @@
+const textArea = document.querySelector('#textarea')
+const totalCounter = document.querySelector('#total-counter')
+const remainingCounter = document.querySelector('#remaining-counter')
+
